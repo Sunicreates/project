0b536e9b95a5003a35909868c505851caf4b514e
Sunicreates <Sunilkumarerere@gmail.com>
2025-06-16 22:04:29 -0700

    Fix all CSS configuration problems

--- roadrage/src/index.js
+++ roadrage/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './Components/j.js/index.css';
-
-
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,4 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
